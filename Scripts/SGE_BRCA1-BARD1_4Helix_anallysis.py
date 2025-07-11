88a296bec7da42f168c03bcc6d288d4f72cd5485
MusicalSquire <ivanw314@uw.edu>
2025-07-11 16:42:22 -0700

    Initial data processing

--- Scripts/SGE_BRCA1-BARD1_4Helix_anallysis.py
+++ Scripts/SGE_BRCA1-BARD1_4Helix_anallysis.py
@@ -15,11 +15,19 @@ bard1_helix_residues = list(range(24, 49)) + list(range(98, 118))
 brca1_helix_residues = list(range(7, 23)) + list(range(80, 98))
 
 bard1_data = bard1_data.rename(columns = {'simplified_consequence': 'Consequence'})
+brca1_data = brca1_data.rename(columns = {'snv_score_minmax': 'score'})
 bard1_data = bard1_data.loc[bard1_data['Consequence'].isin(['missense_variant'])]
+brca1_data = brca1_data.loc[brca1_data['Consequence'].isin(['missense_variant'])]
 
 bard1_data['AApos'] = bard1_data['amino_acid_change'].transform(lambda x: x[1:-1])
-#brca1_data['AApos'] = brca1_data['amino_acid_change'].transform(lambda x: x[1:-1])
 
-#brca1_data = brca1_data.loc[(brca1_data['AApos'].astype(int).isin(brca1_helix_residues)) & (brca1_data['Consequence'] == 'missense_variant')]
+brca1_data['AApos'] = brca1_data['hgvs_pro'].transform(lambda x: x.split(':')[1].split('.')[1][3:-3])
 
-print(bard1_data.head())
+bard1_data = bard1_data.loc[bard1_data['AApos'].astype(int).isin(bard1_helix_residues)]
+brca1_data = brca1_data.loc[brca1_data['AApos'].astype(int).isin(brca1_helix_residues)]
+
+
+bard1_collapsed = bard1_data.groupby('AApos').agg({'score': 'median'}).reset_index()
+brca1_collapsed = brca1_data.groupby('AApos').agg({'score': 'median'}).reset_index()
+
+print(brca1_collapsed.head())
