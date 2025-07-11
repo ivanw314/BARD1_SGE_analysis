dc209ac0700ea0e2c42e11e8e1ea7cefaee30a8a
MusicalSquire <ivanw314@uw.edu>
2025-07-11 16:05:00 -0700

    Create SGE_BRCA1-BARD1_4Helix_anallysis.py
    Initial Commit

---
+++ Scripts/SGE_BRCA1-BARD1_4Helix_anallysis.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+from matplotlib.colors import LinearSegmentedColormap
+from pymol import cmd
+
+bard1_file = '/Users/ivan/Documents/GitHub/BARD1_SGE_analysis/Data/20250508_BARD1scores_update_FILTERED.xlsx'
+brca1_file = '/Users/ivan/Documents/GitHub/BARD1_SGE_analysis/Data/20240830_BRCA1_SGE_AllScores.xlsx'
+
+bard1_data = pd.read_excel(bard1_file)
+brca1_data = pd.read_excel(brca1_file)
+
+#Helix residues for BARD1: [24, 48] and [98, 117]
+#Helix residues for BRCA1: [7, 22] and [80, 97]
+bard1_helix_residues = list(range(24, 49)) + list(range(98, 118))
+brca1_helix_residues = list(range(7, 23)) + list(range(80, 98))
+
+bard1_data = bard1_data.rename(columns = {'simplified_consequence': 'Consequence'})
+bard1_data = bard1_data.loc[bard1_data['Consequence'].isin(['missense_variant'])]
+
+bard1_data['AApos'] = bard1_data['amino_acid_change'].transform(lambda x: x[1:-1])
+#brca1_data['AApos'] = brca1_data['amino_acid_change'].transform(lambda x: x[1:-1])
+
+#brca1_data = brca1_data.loc[(brca1_data['AApos'].astype(int).isin(brca1_helix_residues)) & (brca1_data['Consequence'] == 'missense_variant')]
+
+print(bard1_data.head())
