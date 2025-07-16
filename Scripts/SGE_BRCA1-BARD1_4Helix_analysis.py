772ace75b4d36fc0226bfc8f5a9fc960a97795b5
MusicalSquire <ivanw314@uw.edu>
2025-07-16 10:50:54 -0700

    4Helix renamed and updated
    Updated to show indeterminate scores in yellow. Code commented out

--- Scripts/SGE_BRCA1-BARD1_4Helix_analysis.py
+++ Scripts/SGE_BRCA1-BARD1_4Helix_analysis.py
@@ -12,26 +12,26 @@ type = 'min_NP'  # 'min', 'mean', 'min_NP', 'mean_NP' for minimum, mean score, o
 chain_info = {'BARD1': 'B', 'BRCA1': 'A'}   #Specify chain identifiers for BARD1 and BRCA1
 
 def read_process_data(bard1_file, brca1_file, type, chains=chain_info):
-    bard1_data = pd.read_excel(bard1_file)
-    brca1_data = pd.read_excel(brca1_file)
+    bard1_data = pd.read_excel(bard1_file) #Reads BARD1 data from Excel file
+    brca1_data = pd.read_excel(brca1_file) #Reads BRCA1 data from Excel file
 
     #Helix residues for BARD1: [34, 48] and [98, 117]
     #Helix residues for BRCA1: [7, 22] and [80, 97]
-    bard1_helix_residues = list(range(26, 48)) + list(range(98, 123))
-    brca1_helix_residues = list(range(7, 23)) + list(range(80, 98))
+    bard1_helix_residues = list(range(26, 48)) + list(range(98, 123)) #BARD1 helix residues
+    brca1_helix_residues = list(range(7, 23)) + list(range(80, 98)) #BRCA1 helix residues
 
-    bard1_data = bard1_data.rename(columns = {'simplified_consequence': 'Consequence'})
-    brca1_data = brca1_data.rename(columns = {'snv_score_minmax': 'score'})
-    bard1_data = bard1_data.loc[bard1_data['Consequence'].isin(['missense_variant'])]
-    brca1_data = brca1_data.loc[brca1_data['Consequence'].isin(['missense_variant'])]
+    bard1_data = bard1_data.rename(columns = {'simplified_consequence': 'Consequence'}) #Renames column for consistency
+    brca1_data = brca1_data.rename(columns = {'snv_score_minmax': 'score'}) #Renames column for consistency
+    bard1_data = bard1_data.loc[bard1_data['Consequence'].isin(['missense_variant'])] #Pulls only missense variants for BARD1
+    brca1_data = brca1_data.loc[brca1_data['Consequence'].isin(['missense_variant'])] #pulls only missense variants for BRCA1
 
-    bard1_data['AApos'] = bard1_data['amino_acid_change'].transform(lambda x: x[1:-1])
+    bard1_data['AApos'] = bard1_data['amino_acid_change'].transform(lambda x: x[1:-1]) #Gets amino acid position from the amino acid change
 
-    brca1_data['AApos'] = brca1_data['hgvs_pro'].transform(lambda x: x.split(':')[1].split('.')[1][3:-3])
+    brca1_data['AApos'] = brca1_data['hgvs_pro'].transform(lambda x: x.split(':')[1].split('.')[1][3:-3]) #Gets amino acid position from the HGVS protein change
+    bard1_data = bard1_data.loc[bard1_data['AApos'].astype(int).isin(bard1_helix_residues)] #pulls helix residues for BARD1
+    brca1_data = brca1_data.loc[brca1_data['AApos'].astype(int).isin(brca1_helix_residues)] #pulls helix residues for BRCA1
 
-    bard1_data = bard1_data.loc[bard1_data['AApos'].astype(int).isin(bard1_helix_residues)]
-    brca1_data = brca1_data.loc[brca1_data['AApos'].astype(int).isin(brca1_helix_residues)]
-
+    #Aggregate data based on the specified type
     if type == 'min':
         bard1_collapsed = bard1_data.groupby('AApos').agg({'score': 'min'}).reset_index()
         brca1_collapsed = brca1_data.groupby('AApos').agg({'score': 'min'}).reset_index()
@@ -60,11 +60,12 @@ def read_process_data(bard1_file, brca1_file, type, chains=chain_info):
         bard1_collapsed = bard1_data.groupby('AApos').agg({'score': 'mean'}).reset_index()
         brca1_collapsed = brca1_data.groupby('AApos').agg({'score': 'mean'}).reset_index()
 
-    bard1_collapsed['median_consequence'] = 'intermediate'
+    #pulls indeterminate, normal, and abnormal residues based on the cutoffs
+    bard1_collapsed['median_consequence'] = 'indeterminate'
     bard1_collapsed.loc[bard1_collapsed['score'] <= bard1_cutoffs[0], 'median_consequence'] = 'functionally_abnormal'
     bard1_collapsed.loc[bard1_collapsed['score'] >= bard1_cutoffs[1], 'median_consequence'] = 'functionally_normal'
 
-    brca1_collapsed['median_consequence'] = 'intermediate'
+    brca1_collapsed['median_consequence'] = 'indeterminate'
     brca1_collapsed.loc[brca1_collapsed['score'] <= brca1_cutoffs[0], 'median_consequence'] = 'functionally_abnormal'
     brca1_collapsed.loc[brca1_collapsed['score'] >= brca1_cutoffs[1], 'median_consequence'] = 'functionally_normal'
 
@@ -74,21 +75,29 @@ def read_process_data(bard1_file, brca1_file, type, chains=chain_info):
     bard1_normal = bard1_collapsed.loc[bard1_collapsed['median_consequence'] == 'functionally_normal']
     brca1_normal = brca1_collapsed.loc[brca1_collapsed['median_consequence'] == 'functionally_normal']
 
+    bard1_indeterminate = bard1_collapsed.loc[bard1_collapsed['median_consequence'] == 'indeterminate']
+    brca1_indeterminate = brca1_collapsed.loc[brca1_collapsed['median_consequence'] == 'indeterminate']
+
     bard1_abnormal_residues = bard1_abnormal['AApos'].astype(int).tolist()
     brca1_abnormal_residues = brca1_abnormal['AApos'].astype(int).tolist()
 
     bard1_normal_residues = bard1_normal['AApos'].astype(int).tolist()
     brca1_normal_residues = brca1_normal['AApos'].astype(int).tolist()
 
+    bard1_indeterminate_residues = bard1_indeterminate['AApos'].astype(int).tolist()
+    brca1_indeterminate_residues = brca1_indeterminate['AApos'].astype(int).tolist()
+
     to_return = {'BARD1_abnormal': (bard1_abnormal_residues, chain_info['BARD1']),
                  'BARD1_normal': (bard1_normal_residues, chain_info['BARD1']),
                  'BRCA1_abnormal': (brca1_abnormal_residues, chain_info['BRCA1']),
                  'BRCA1_normal': (brca1_normal_residues, chain_info['BRCA1']),
-               }
+                 'BARD1_indeterminate': (bard1_indeterminate_residues, chain_info['BARD1']),
+                 'BRCA1_indeterminate': (brca1_indeterminate_residues, chain_info['BRCA1']),
+               } #Final dictionary with residues categorized by their consequence
 
     return to_return
 
-def generate_selection_string(residues, selection="(all)", chain = chain_info):
+def generate_selection_string(residues, selection="(all)", chain = chain_info): #Generate selection strings for residues
     """
     Generate a PyMOL selection string for the specified residues.
     Parameters:
@@ -114,7 +123,7 @@ def generate_selection_string(residues, selection="(all)", chain = chain_info):
         elif 'normal' in key:
             color = 'cyan'
         else:
-            color = 'gray'
+            color = 'yellow'  # For indeterminate residues
         
         # Create selection name for clarity
         sel_name = f"sel_{key}_{chain}"
